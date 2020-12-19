3a890ed2ea6e5e9710d45837a37173107b806e61
ryuhei.tashiro <ryu9gd29@gmail.com>
2020-12-19 15:51:50 +0900

    課題2-4
    2-4の作成

---
+++ JavaStudy/2-4/2-4/src/study/Study.java
@@ -0,0 +1,19 @@
+package study;
+
+import constants.FruitsConstants;
+import fruits.Fruits;
+
+public class Study {
+
+	public static void main(String[] args) {
+		/* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+         *     また、課題の画像と同じ表示になるように
+         *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
+         */
+		Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
+
+
+
+	}
+
+}
